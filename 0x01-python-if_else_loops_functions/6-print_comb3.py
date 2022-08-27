3155c2b9b03bd3e64cfbf02a0fc143a61d173ac0
Noxolo <Noxoloshlatshwayo@gmail.com>
2022-08-27 07:56:45 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for d1 in range(0, 9):
     for d2 in range(d1 + 1, 10):
         if d1 == 8:
-            print("{:d}{:d}".format(d1,d2))
+            print("{:d}{:d}".format(d1, d2))
             break
-        print("{:d}{:d}".format(d1,d2), end=", ")
+        print("{:d}{:d}".format(d1, d2), end=", ")
